696f6080a958ad69d01e98f585a7117dd29933c9
filtr-news <ah1318+filtr@gmail.com>
2026-06-18 11:25:29 -0400

    Update types.ts

--- lib/types.ts
+++ lib/types.ts
@@ -27,7 +27,6 @@ export type FiltrReport = {
   summary: string[];
   keyFacts: {
     verifiableClaims: string[];
-    importantNumbers: string[];
     namedEntities: string[];
   };
   whatActuallyMatters: {
